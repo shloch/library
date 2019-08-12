3958892c18ef2cf86b2de22a8488ed0844dba907
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-08-12 10:45:22 +0100

    Project skeleton

---
+++ asset/library.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//alert("test");
+
+let myLibrary = [];
+
+function Book(author, title, numPages, status = false) {
+    this.author = author;
+    this.title = title;
+    this.numPages = numPages;
+    this.status = status;
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
